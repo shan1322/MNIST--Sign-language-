81dcdcc197fc9e946cac7345adfbf3bc544f5d81
Shantanu Singh <shaneyasingh13@gmail.com>
2018-07-21 11:17:21 +0530

    readme fixed

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # MNIST Sign Classifier
 A Image Classifier based on custom Architecture
+
+
+
+![Input Image](images/sample.PNG)
+![Output Captiion](images/out.png)
+
+
+
+
+
 Dataset from [https://www.kaggle.com/datamunge/sign-language-mnist](https://www.kaggle.com/datamunge/sign-language-mnist)
--- integrate.py
+++ integrate.py
@@ -4,6 +4,11 @@ from matplotlib.pyplot import imshow
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+loaded_model.load_weights("model.h5")
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 if vc.isOpened():
@@ -13,6 +18,8 @@ else:
 while rval:
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
+    image=cv2.resize(frame,(28,28))
+    print(loaded_model.predict(np.expand_dims(np.array(image),axis=0)))
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         break
